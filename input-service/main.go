f6c5aa1f72beebb52f7b93b0ed768743e1cfdc2d
Thiago <thiago.dev.x@gmail.com>
2025-06-20 20:17:07 -0300

    update

--- input-service/main.go
+++ input-service/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/black-dev-x/go-lab-cep-temperature/config"
 )
@@ -22,6 +25,21 @@ func main() {
 			http.Error(w, "invalid zipcode", 422)
 			return
 		}
+
+		json, _ := json.Marshal(input)
+
+		req, _ := http.NewRequest("POST", os.Getenv("PROCESSING_SERVICE_URL"), bytes.NewBuffer(json))
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			http.Error(w, "failed to send request", 500)
+			return
+		}
+		defer resp.Body.Close()
+
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
 	})
 	http.ListenAndServe(":3000", nil)
 }
